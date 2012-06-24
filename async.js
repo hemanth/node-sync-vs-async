09a853791ec775fa9f47e495cc4fa162d770e476
hemanth.hm <hemanth.hm@gmail.com>
2012-06-24 19:02:25 +0530

    Added async code

---
+++ async.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+var contents = fs.readFile('./users','utf8', function(err,contents){
+   console.log(contents);
+});
+
+console.log("Hello Node");
+
+var contents = fs.readFile('./hosts','utf8' function(err,contents){
+   console.log(contents);
+});
+console.log("Hello again!");
+
