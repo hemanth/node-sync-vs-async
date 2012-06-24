a52adfe6f425954fc5f7cd250597df7ec11f62ef
hemanth.hm <hemanth.hm@gmail.com>
2012-06-24 19:55:07 +0530

    Added \n for beauty

--- sync.js
+++ sync.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 
 var contents = fs.readFileSync('users','utf8');
 console.log(contents);
-console.log("Hello Node");
+console.log("Hello Node\n");
 
 var contents = fs.readFileSync('hosts','utf8');
 console.log(contents);
