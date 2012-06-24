7d49fce533ab54ef4e4fe44beef0fb095a3030d7
hemanth.hm <hemanth.hm@gmail.com>
2012-06-24 19:40:33 +0530

    Added final cut of async

--- async.js
+++ async.js
@@ -3,10 +3,10 @@ var fs = require('fs');
 var contents = fs.readFile('./users','utf8', function(err,contents){
    console.log(contents);
 });
-
 console.log("Hello Node");
 
-var contents = fs.readFile('./hosts','utf8' function(err,contents){
+
+var contents = fs.readFile('./hosts','utf8', function(err,contents){
    console.log(contents);
 });
 console.log("Hello again!");
